56ca48471e4b3a4098772b613117d00bb4bc29a0
xinzai <1213657724@qq.com>
2018-07-05 19:34:35 +0800

    new file

---
+++ resource.h
@@ -0,0 +1,52 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by FaceDetect.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FACEDETECT_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_REPLACE              129
+#define IDI_ICON1                       130
+#define IDI_ICON_HALLOWEEN1             131
+#define IDI_ICON_HALLOWEEN2             132
+#define IDI_ICON_OPEN                   133
+#define IDI_ICON_LIKEHOOD               134
+#define IDI_ICON_BINARY                 135
+#define IDI_ICON_MARK_FACE              136
+#define IDI_ICON_EDGE                   137
+#define IDI_ICON_MARK_NOSE              138
+#define IDI_ICON_MARK_MOUSE             139
+#define IDI_ICON_MARK_MOUSE2            141
+#define IDI_ICON_MARK_FACE2             142
+#define IDI_ICON_BINARY2                144
+#define IDI_ICON_MARK_NOSE2             145
+#define IDC_BMPSHOW                     1000
+#define IDC_BTN_OPENFILE                1001
+#define IDC_BTN_LIKEHOOD                1002
+#define IDC_BTN_BINARY                  1003
+#define IDC_BTN_HISTOGRAM_V             1004
+#define IDC_BTN_HISTOGRAM_H             1005
+#define IDC_BTN_MARK_FACE_1             1006
+#define IDC_BTN_FACEHAIR                1007
+#define IDC_BTN_HISTOGRAM_FACE          1008
+#define IDC_BTN_HISTOGRAM_HAIR          1009
+#define IDC_BTN_MARK_FACE_2             1010
+#define IDC_BTN_EDGE                    1011
+#define IDC_BTN_MARK_EYE                1012
+#define IDC_BTN_MARK_MOUSE              1013
+#define IDC_BTN_MARK_NOSE               1014
+#define IDC_BUTTON1                     1017
+#define IDC_STATIC_SCORE                1018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        146
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
